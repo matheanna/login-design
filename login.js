34cd2a5fa7e917da15aedf43bb7cd722aae22f6b
matheanna <113627409+matheanna@users.noreply.github.com>
2023-12-01 10:47:36 +0100

    toggle hight

--- login.js
+++ login.js
@@ -59,10 +59,14 @@ const passwordInput = root.querySelector('#password');
 document.addEventListener('DOMContentLoaded', function () {
     const downloadBtn = root.querySelector('#download-btn');
     const formAndLogo = root.querySelector('#form-and-logo');
+    const form = root.querySelector("#form-conatiner");
+    const logo = root.querySelector("#logo-container");
     const qrCodes = root.querySelector('#qr-codes');
 
     downloadBtn.addEventListener('click', function () {
         formAndLogo.classList.toggle('expanded');
+        form.classList.toggle('expanded');
+        logo.classList.toggle('expanded');
         const isExpanded = formAndLogo.classList.contains('expanded');
         qrCodes.style.display = isExpanded ? 'flex' : 'none';
     });
