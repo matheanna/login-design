c1e1a6fdf9be3a55aff5ca0507ae5d13ee417d94
matheanna <113627409+matheanna@users.noreply.github.com>
2023-12-01 12:56:00 +0100

    move placeholders up

--- login.js
+++ login.js
@@ -71,3 +71,24 @@ document.addEventListener('DOMContentLoaded', function () {
         qrCodes.style.display = isExpanded ? 'flex' : 'none';
     });
 });
+
+//move placeholders up
+document.addEventListener('DOMContentLoaded', function () {
+    const usernameInput = root.querySelector('#username');
+    const passwordInput = root.querySelector('#password');
+
+    function handleInputFocus(input) {
+        input.addEventListener('input', function () {
+            if (input.value.trim() !== '') {
+                input.classList.add('form-input-filled');
+            } else {
+                input.classList.remove('form-input-filled');
+            }
+        });
+    }
+
+    handleInputFocus(usernameInput);
+    handleInputFocus(passwordInput);
+});
+
+
